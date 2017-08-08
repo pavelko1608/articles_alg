e0a2e60b86dacf09e6530154bfae58bb510c82e0
Pavlo Paliychuk <pavelko1608@gmail.com>
2017-08-08 15:01:42 +0300

    added fox news

--- article.py
+++ article.py
@@ -27,7 +27,7 @@ def cnn_article():
 		article.parse()
 		article_list.append(article.text)
 		ctr += 1
-		progress(ctr, 839)
+		progress(ctr, 839, status = "Fetching CNN")
 	with open('cnn_articles.pkl', 'wb') as f:
 		pickle.dump(article_list, f)
 
@@ -35,9 +35,31 @@ def cnn_article():
 		newlist = pickle.load(f)
 	print("cnn len:")
 	print len(newlist)
-	elapsed_time = time.time() - start_time
-	print elapsed_time, "seconds elapsed"
+	elapsed_time = (time.time() - start_time) / 60
+	print elapsed_time, "minutes elapsed"
 
 
+def fox_article():
+	start_time = time.time()
+	article_list = []
+	fox_paper = newspaper.build("http://nation.foxnews.com/", memoize_articles=False)
+	ctr = 0
+	for article in fox_paper.articles:
+		article.download()
+		article.parse()
+		article_list.append(article.text)
+		ctr += 1
+		progress(ctr, 371, status = "Fetching Fox news")
+	with open('fox_articles.pkl', 'wb') as f:
+		pickle.dump(article_list, f)
+
+	with open('fox_articles.pkl', 'rb') as f:
+		newlist = pickle.load(f)
+	print("fox len:")
+	print len(newlist)
+	elapsed_time = (time.time() - start_time) / 60
+	print elapsed_time, "minutes elapsed"
+
+fox_article()
 cnn_article()
 
