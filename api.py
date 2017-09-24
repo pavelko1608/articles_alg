6e5fbb02cd087fe1e89f35e6a682655be48bab1b
Pavlo Paliychuk <pavelko1608@gmail.com>
2017-09-24 19:33:24 +0300

    added run thing

--- api.py
+++ api.py
@@ -1,23 +1,25 @@
+import pickle
 from flask import Flask
 from flask import request
-app = Flask(__name__)
-from sklearn.feature_extraction.text import TfidfVectorizer
-import pickle
 from index import vectorizer, filename
-from sklearn.feature_extraction.text import TfidfVectorizer
-import pickle
 from goose import Goose
 from requests import get
 
+app = Flask(__name__)
+
+
 @app.route('/api/get_response', methods=["POST"])
 def send_response():
-	response = get(request.data)
-	extractor = Goose()
-	article = extractor.extract(raw_html=response.content)
-	text = [article.cleaned_text]
-	data_vector = vectorizer.transform(text)
-	loaded_model = pickle.load(open(filename, 'rb'))
-	if (loaded_model.predict(data_vector) == [0]):
-		return "Left wing"
-	if (loaded_model.predict(data_vector) == [1]):
-		return "Right wing"
+    response = get(request.data)
+    extractor = Goose()
+    article = extractor.extract(raw_html=response.content)
+    text = [article.cleaned_text]
+    data_vector = vectorizer.transform(text)
+    loaded_model = pickle.load(open(filename, 'rb'))
+    if loaded_model.predict(data_vector) == [0]:
+        return "Left wing"
+    if loaded_model.predict(data_vector) == [1]:
+        return "Right wing"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')        
--- index.py
+++ index.py
@@ -3,66 +3,67 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 import pickle
-from nltk.stem.snowball import SnowballStemmer
+#from nltk.stem.snowball import SnowballStemmer
 from feature_format import featureFormat, targetFeatureSplit
-from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import train_test_split
 import time
 
 cnn_data = []
 with open('cnn_articles.pkl', 'rb') as f:
-		cnn = pickle.load(f)
+    cnn = pickle.load(f)
 for article in cnn:
-	cnn_data.append([0, article])
+    cnn_data.append([0, article])
 
 vox_data = []
 with open('vox_articles.pkl', 'rb') as f:
-		vox = pickle.load(f)
+    vox = pickle.load(f)
 for article in vox:
-	vox_data.append([0, article])
+    vox_data.append([0, article])
 
 politico_data = []
 with open('politico_articles.pkl', 'rb') as f:
-		politico = pickle.load(f)
+    politico = pickle.load(f)
 for article in politico:
-	politico_data.append([0, article])
+    politico_data.append([0, article])
 
 washington_post_data = []
 with open('washington_post_articles.pkl', 'rb') as f:
-		washington_post = pickle.load(f)
+    washington_post = pickle.load(f)
 for article in washington_post:
-	washington_post_data.append([0, article])		
+    washington_post_data.append([0, article])
 
 left_wing_data = cnn_data + vox_data + politico_data + washington_post_data
 
 fox_data = []
 with open('fox_articles.pkl', 'rb') as f:
-		fox = pickle.load(f)
+    fox = pickle.load(f)
 for article in fox:
-	fox_data.append([1, article])
+    fox_data.append([1, article])
 
 observer_data = []
 with open('observer_articles.pkl', 'rb') as f:
-		observer = pickle.load(f)
+    observer = pickle.load(f)
 for article in observer:
-	observer_data.append([1, article])
+    observer_data.append([1, article])
 
 breitbart_data = []
 with open('breitbart_articles.pkl', 'rb') as f:
-		breitbart = pickle.load(f)
+    breitbart = pickle.load(f)
 for article in breitbart:
-	breitbart_data.append([1, article])	
+    breitbart_data.append([1, article])
 
 newsmax_data = []
 with open('newsmax_articles.pkl', 'rb') as f:
-		newsmax = pickle.load(f)
+    newsmax = pickle.load(f)
 for article in newsmax:
-	newsmax_data.append([1, article])				
+    newsmax_data.append([1, article])
 
 right_wing_data = fox_data + observer_data + breitbart_data + newsmax_data
 
 data = left_wing_data + right_wing_data
 labels, articles = targetFeatureSplit(data)
-articles_train, articles_test, labels_train, labels_test = train_test_split(articles, labels, test_size = 0.3, random_state = 42)
+articles_train, articles_test, labels_train, labels_test = train_test_split(articles, labels, test_size=0.001,
+                                                                            random_state=42)
 
 flat_train = []
 flat_test = []
@@ -72,14 +73,14 @@ for sublist in articles_train:
         flat_train.append(article)
 for sublist in articles_test:
     for article in sublist:
-        flat_test.append(article)  
+        flat_test.append(article)
 
-vectorizer = TfidfVectorizer(strip_accents = "unicode", lowercase = False)
+vectorizer = TfidfVectorizer(strip_accents="unicode", lowercase=False)
 vectors = vectorizer.fit_transform(flat_train)
 vectors_test = vectorizer.transform(flat_test)
 
 t0 = time.time()
-clf = MultinomialNB(alpha=.01, fit_prior = False)
+clf = MultinomialNB(alpha=.01, fit_prior=False)
 clf.fit(vectors, labels_train)
 pred = clf.predict(vectors_test)
 
@@ -100,10 +101,10 @@ pickle.dump(clf, open(filename, 'wb'))
 # print "f1_score:", metrics.f1_score(labels_test, pred, average='micro')
 # print "recall_score:", metrics.recall_score(labels_test, pred, average='micro')
 # print "precision_score:", metrics.precision_score(labels_test, pred)
-# print "assessed correctly:", metrics.accuracy_score(labels_test, pred, normalize = False)
-# print "assessed incorrectly:", len(flat_test) - metrics.accuracy_score(labels_test, pred, normalize = False)
+# print "assessed correctly:", metrics.accuracy_score(labels_test, pred, normalize=False)
+# print "assessed incorrectly:", len(flat_test) - metrics.accuracy_score(labels_test, pred, normalize=False)
 # print "accuracy:", metrics.accuracy_score(labels_test, pred)
-# print (time.time() - t0) / 60, "mins elapsed"
+# print (time.time() - t0) / 60, "minutes elapsed"
 #     BEST SCORES
 # Number of samples: 1306
 # f1_score: 0.914241960184
@@ -113,8 +114,3 @@ pickle.dump(clf, open(filename, 'wb'))
 # assessed incorrectly: 112
 # accuracy: 0.914241960184
 # 6.14256568352 mins elapsed
-
-
-
-
-
